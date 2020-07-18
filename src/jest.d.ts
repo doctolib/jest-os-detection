7366f759288683bdcd8ec480f0fa6d4b82130149
Quentin Ménoret <quentin.menoret@doctolib.com>
2020-07-18 14:25:51 +0200

    prettier

--- src/jest.d.ts
+++ src/jest.d.ts
@@ -1,20 +1,19 @@
-
 declare module jest {
   interface Describe {
-    onWindows: jest.Describe | jest.It,
-    onMac: jest.Describe | jest.It,
-    onLinux: jest.Describe | jest.It,
-    skipWindows: jest.Describe | jest.It,
-    skipMac: jest.Describe | jest.It,
+    onWindows: jest.Describe | jest.It
+    onMac: jest.Describe | jest.It
+    onLinux: jest.Describe | jest.It
+    skipWindows: jest.Describe | jest.It
+    skipMac: jest.Describe | jest.It
     skipLinux: jest.Describe | jest.It
   }
 
   interface It {
-    onWindows: jest.Describe | jest.It,
-    onMac: jest.Describe | jest.It,
-    onLinux: jest.Describe | jest.It,
-    skipWindows: jest.Describe | jest.It,
-    skipMac: jest.Describe | jest.It,
+    onWindows: jest.Describe | jest.It
+    onMac: jest.Describe | jest.It
+    onLinux: jest.Describe | jest.It
+    skipWindows: jest.Describe | jest.It
+    skipMac: jest.Describe | jest.It
     skipLinux: jest.Describe | jest.It
   }
 
--- src/patch-jest.ts
+++ src/patch-jest.ts
@@ -1,19 +1,18 @@
-
 type JestTestOrSuite = jest.Describe | jest.It
 
 enum Platform {
   win32 = 'win32',
   darwin = 'darwin',
-  linux = 'linux'
+  linux = 'linux',
 }
 
 interface Command {
-  onWindows: [Platform.win32],
-  onMac: [Platform.darwin],
-  onLinux: [Platform.linux],
-  skipWindows: [Platform.linux, Platform.darwin],
-  skipMac: [Platform.linux, Platform.win32],
-  skipLinux: [Platform.darwin, Platform.win32],
+  onWindows: [Platform.win32]
+  onMac: [Platform.darwin]
+  onLinux: [Platform.linux]
+  skipWindows: [Platform.linux, Platform.darwin]
+  skipMac: [Platform.linux, Platform.win32]
+  skipLinux: [Platform.darwin, Platform.win32]
 }
 
 const AVAILABLE_PLATFORMS: Command = {
@@ -30,7 +29,7 @@ type MethodOperation = 'skip' | 'only'
 const methodOperations: MethodOperation[] = ['skip', 'only']
 
 export function patch(currentPlatform: Platform) {
-  function newDefinition<T>(method: T, expectedPlatform: keyof Command, fallbackImplem: T ) : T {
+  function newDefinition<T>(method: T, expectedPlatform: keyof Command, fallbackImplem: T): T {
     if ((AVAILABLE_PLATFORMS[expectedPlatform] as Platform[] | undefined)?.indexOf(currentPlatform) !== -1) {
       return method
     } else {
@@ -38,12 +37,12 @@ export function patch(currentPlatform: Platform) {
     }
   }
 
-  (Object.keys(AVAILABLE_PLATFORMS) as (keyof Command)[]).forEach(platform => {
+  ;(Object.keys(AVAILABLE_PLATFORMS) as (keyof Command)[]).forEach(platform => {
     ;[describe, it, test].forEach(method => {
       method[platform] = newDefinition<JestTestOrSuite>(method, platform, method.skip)
       method[platform].each = newDefinition<jest.Each>(method.each, platform, method.skip.each)
 
-      methodOperations.forEach((mode) => {
+      methodOperations.forEach(mode => {
         method[platform][mode] = newDefinition<JestTestOrSuite>(method[mode], platform, method.skip)
         method[platform][mode].each = newDefinition<jest.Each>(method[mode].each, platform, method.skip.each)
       })
